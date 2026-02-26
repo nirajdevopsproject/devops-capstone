6ecba4356bbb1e1005244515f13993f3c88ae0ea
nirajdevopsproject <naughtyboy894143.com>
2026-02-26 12:41:13 +0000

    app.py added

--- app/app.py
+++ app/app.py
@@ -4,30 +4,21 @@ import os
 
 app = Flask(__name__)
 
+def get_connection():
+    return pymysql.connect(
+        host=os.environ.get("DB_HOST"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASS"),
+        database=os.environ.get("DB_NAME")
+    )
+
 @app.route("/")
 def home():
-    connection = pymysql.connect(
-        host=os.environ.get("DB_HOST"),
-        user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASS"),
-        database=os.environ.get("DB_NAME")
-    )
+    return "App is running!"
 
-    cursor = connection.cursor()
-    cursor.execute("SELECT VERSION();")
-    data = cursor.fetchone()
-    connection.close()
-
-    return f"MySQL Version: {data}"
 @app.route("/data")
 def show_data():
-    connection = pymysql.connect(
-        host=os.environ.get("DB_HOST"),
-        user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASS"),
-        database=os.environ.get("DB_NAME")
-    )
-
+    connection = get_connection()
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM test_table;")
     data = cursor.fetchall()
